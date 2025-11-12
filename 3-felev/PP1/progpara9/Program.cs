e8d726e9bb802abefee3204b7696e38d6fd7aa37
Túri Balázs <106369596+TuriBalazs@users.noreply.github.com>
2025-11-12 13:31:23 +0100

    Add files via upload

---
+++ 3-felev/PP1/progpara9/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace progpara9
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            List<int> eredmenyek = new List<int>() {444, 666, 555 };
+            Versenyzo versenyzok = new Versenyzo(0, "Ádám", eredmenyek);
+            Console.WriteLine(versenyzok);
+
+            versenyzok.SetID(1);
+            Console.WriteLine(versenyzok);
+            versenyzok.SetNev("Tamás");
+            Console.WriteLine(versenyzok);
+            Versenyzo v;
+            List<Versenyzo> verseny = new List<Versenyzo>();
+            Console.WriteLine();
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\4-110-4\\source\\repos\\progpara9\\progpara9\\adatok.txt");
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] d = line.Split();
+                    int id = int.Parse(d[0].Substring(1));
+                    string nev = $"{d[1]} {d[2]}";
+                    List<int> list = new List<int>();
+                    for (int i = 3; i < d.Length; i++)
+                    {
+                        int x= int.Parse(d[i]);
+                        list.Add(x);
+                    }
+                    v = new Versenyzo(id, nev, list);
+                    verseny.Add(v);
+                }
+                sr.Close();
+                foreach (var x in verseny)
+                {
+                    Console.WriteLine(x);
+                }
+
+                verseny.Sort();
+                Console.WriteLine("Rendezett:");
+                foreach (var x in verseny)
+                {
+                    Console.WriteLine(x);
+                }
+
+                Console.WriteLine("Azonos nevü: ");
+                for (int i = 0; i < verseny.Count; i++)
+                {
+                    for (int j = i + 1; j < verseny.Count; j++)
+                    {
+                        if (verseny[i].GetNev() == verseny[j].GetNev())
+                        {
+                            Console.WriteLine(verseny[i]);
+                            Console.WriteLine(verseny[j]);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ 3-felev/PP1/progpara9/Versenyzo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progpara9
+{
+    internal class Versenyzo : IComparable<Versenyzo>
+    {
+        int ID;
+        string Nev;
+        List<int> Eredmenyek;
+
+        public Versenyzo(int id, string nev, List<int> eredmenyek)
+        {
+            this.ID = id;
+            this.Nev = nev;
+            this.Eredmenyek = eredmenyek;
+        }
+
+        public Versenyzo()
+        {
+            Eredmenyek = new List<int> { };
+        }
+        public int GetID() { return ID; }
+        public string GetNev() { return Nev; }
+        public void SetID(int id) { this.ID = id; }
+        public void SetNev(string nev) { this.Nev = nev; }
+
+        public int legjobbUgras()
+        {
+            int temp = 1;
+            if (Eredmenyek.Count > 0)
+            {
+                for (int i = 0; i < Eredmenyek.Count; i++)
+                {
+                    if (temp < Eredmenyek[i])
+                    {
+                        temp = Eredmenyek[i];
+                    }
+                }
+            }
+            return temp;
+        }
+
+        public override string? ToString()
+        {
+            string szoveg = $"ID: {ID}, Név: {Nev}, Eredmenyek: ";
+            for (int i = 0; i < Eredmenyek.Count; i++)
+            {
+                szoveg += $"{ Eredmenyek[i]}, ";
+            }
+            
+            szoveg += $"Legjobb: {legjobbUgras()}";
+            return szoveg;
+        }
+
+        public int CompareTo(Versenyzo? other)
+        {
+            return other.legjobbUgras().CompareTo(this.legjobbUgras());
+        }
+    }
+}
