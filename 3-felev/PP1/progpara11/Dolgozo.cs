78c73ecdd785b54ab410d1ed1df0a7fd1606733c
Túri Balázs <106369596+TuriBalazs@users.noreply.github.com>
2025-11-28 17:43:54 +0100

    Add comments to Dolgozo class for better understanding
    Added comments to clarify class functionality and properties.

--- 3-felev/PP1/progpara11/Dolgozo.cs
+++ 3-felev/PP1/progpara11/Dolgozo.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 
 namespace progpara11
 {
-    internal abstract class Dolgozo
+    internal abstract class Dolgozo // az absztrakt classból lehet adatokat származtatni más classokhoz
     {
         protected string nev;
         protected int oraber;
 
-        public string Nev
+        public string Nev // getter, setter: alternativa az értékadáshoz.
         {
             get { return nev; }
             set { if(nev.Length > 5 && nev.Length < 30) nev = value; }
         }
-        public Dolgozo(string nev, int oraber)
+        public Dolgozo(string nev, int oraber) // a konstruktor: csak ez fér hozzá privát és protected változókhoz. ez ad értéket nekik
         {
             this.nev = nev;
             this.oraber = oraber;
@@ -26,3 +26,4 @@ namespace progpara11
         public abstract bool Szabad_e_ekkor(int kezd, int hossz);
     }
 }
+
